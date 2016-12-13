0050c70ecb4a3486339624f5c1944d2b12439e43
Priscila Gonzalez Adrian <pris.18p@gmail.com>
2016-12-13 14:01:57 -0300

    grado

---
+++ Models/Jerarquia.cs
@@ -0,0 +1,17 @@
+namespace Publicaciones.Models
+{
+    public class Jerarquia
+    {
+        public string Nombre { get; set; }
+
+        public string FechaActual { get; set; }
+
+        public int AñoIngreso{get; set; }
+
+        public string Contrato{get; set;}
+        public string Jornada{get; set;}
+        public int HorasDeTrabajo{get; set;}
+        public string TipoAcademico{get; set;}
+        }
+
+    }
