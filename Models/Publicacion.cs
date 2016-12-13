601e27736ffb168d7d400141a4104062abe9ef07
Priscila Gonzalez Adrian <pris.18p@gmail.com>
2016-12-13 13:29:38 -0300

    persona

---
+++ Models/Publicacion.cs
@@ -0,0 +1,14 @@
+namespace Publicaciones.Models
+{
+    public class Publicacion
+    {
+        public string Doi { get; set; }
+        public string Titulo { get; set; }
+        public string Volumen { get; set; }
+        public string PaginaInicio  { get; set; }
+        public string PaginaFinal { get; set; }
+        public string ContifadRechazos { get; set; }
+
+    }
+
+}
