6c2cbb928bffacc93b1dee531b98baca171d62bd
Priscila Gonzalez Adrian <pris.18p@gmail.com>
2016-12-13 13:32:56 -0300

    jerarquia

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -7,7 +7,12 @@ namespace Publicaciones.Models
         public string Nombre { get; set; }
 
         public string Apellido { get; set; }
-    
-    }
 
-}
+        public string Email{get; set; }
+
+        public string FechaNacimiento{get; set;}
+        public string Nacionalidad{get; set;}
+        public string Genero{get; set;}
+        }
+
+    }
